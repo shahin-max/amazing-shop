956e42103ba25eacc255c7e754571278b4696df5
shahin-max <humshaheen786@gmail.com>
2021-08-29 07:43:39 +0530

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:"avasa kurti",
+            category:"top wear",
+            image:'/images/p1.jpg',
+            price:120,
+            brand:"avaasa",
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product",
+
+        },
+        {
+            _id:'2',
+            name:"avasa kurti",
+            category:"top wear",
+            image:'/images/p2.jpg',
+            price:720,
+            brand:"avaasa",
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product",
+
+        },
+        {
+            _id:'3',
+            name:"avasa kurti",
+            category:"top wear",
+            image:'/images/p3.jpg',
+            price:120,
+            brand:"avaasa",
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product",
+
+        },
+        {
+            _id:'4',
+            name:"avasa kurti",
+            category:"top wear",
+            image:'/images/p4.jpg',
+            price:120,
+            brand:"avaasa",
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product",
+
+        },
+        {
+            _id:'5',
+            name:"avasa kurti",
+            category:"top wear",
+            image:'/images/p5.jpg',
+            price:320,
+            brand:"avaasa",
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product",
+
+        },
+        {
+            _id:'6',
+            name:"avasa kurti",
+            category:"top wear",
+            image:'/images/p6.jpg',
+            price:160,
+            brand:"avaasa",
+            rating:4.5,
+            numReviews:10,
+            description:"high quality product",
+
+        },
+    ],
+};
+export default data;
